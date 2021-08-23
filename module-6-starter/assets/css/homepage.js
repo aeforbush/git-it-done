438d81e0daf94c8194c72c1a15814d03c51b3516
aeforbush <87487655+aeforbush@users.noreply.github.com>
2021-08-23 12:50:30 -0600

    added html for user inquery form

--- module-6-starter/assets/css/homepage.js
+++ module-6-starter/assets/css/homepage.js
@@ -4,11 +4,19 @@ var getUserRepos = function(user) {
 
     // make a request to the url
     fetch(apiUrl).then(function(response) {
+        console.log(response);
+        // format the response to json
         response.json().then(function(data) {
             console.log(data);
         })
     })
 };
 
-getUserRepos(); 
+getUserRepos("lernantino"); 
   
+
+
+
+// collect user input to form HTTP requests
+// use an HTTP request's reponse to display data to the user
+// Handle errors that may occur when working with server-side APIs
